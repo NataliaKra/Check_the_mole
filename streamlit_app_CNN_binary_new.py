fe0c2c908b64a50b6a3eff37d6908000f93032b0
Natalia Kravchenko <nmkravchenko@gmail.com>
2021-11-03 16:38:41 -0700

    updated app file

--- streamlit_app_CNN_binary_new.py
+++ streamlit_app_CNN_binary_new.py
@@ -4,7 +4,7 @@ import matplotlib
 from matplotlib import pyplot as plt
 import tensorflow as tf
 import numpy as np
-from tensorflow import keras
+#from tensorflow import keras
 from tensorflow.keras.models import load_model
 from tensorflow.keras import preprocessing
 import time
